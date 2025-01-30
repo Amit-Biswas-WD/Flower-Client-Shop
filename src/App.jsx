65ce4dbcba7e245e1f9f9c6d35913f764a5e99d3
Amit biswas <amitbiswas9282@gmail.com>
2025-01-30 18:45:53 +0600

    all router setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <>
       <div className="flex justify-center gap-10 items-center text-lg font-semibold my-4">
-          <NavLink to={`/`}>Home</NavLink>
-          <NavLink to={`/addUser`}>Add Product</NavLink>
-          <NavLink to={`/allProduct`}>All Product</NavLink>
-          <NavLink to={`/signIn`}>Sign In</NavLink>
-          <NavLink to={`/signUp`}>Sign Up</NavLink>
+        <NavLink to={`/`}>Home</NavLink>
+        <NavLink to={`/addProduct`}>Add Product</NavLink>
+        <NavLink to={`/allProduct`}>All Product</NavLink>
+        <NavLink to={`/signUp`}>Sign Up</NavLink>
+        <NavLink to={`/signIn`}>Sign In</NavLink>
       </div>
     </>
   );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./../App";
+import Home from "../page/Home";
+import AddProduct from "../page/AddProduct";
+import AllProduct from "../page/AllProduct";
+import SignIn from "../page/SignIn";
+import SignUp from "../page/SignUp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/addProduct",
+        element: <AddProduct />,
+      },
+      {
+        path: "/allProduct",
+        element: <AllProduct />,
+      },
+      {
+        path: "/signUp",
+        element: <SignUp />,
+      },
+      {
+        path: "/signIn",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
