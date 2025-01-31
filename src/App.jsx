f6852d943e7954c17ef8e8fadb6190f10cd096b1
Amit biswas <amitbiswas9282@gmail.com>
2025-01-31 19:50:09 +0600

    SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,48 @@ function App() {
   return (
     <div>
       <nav className="flex justify-center gap-10 items-center text-lg font-semibold my-4">
-        <NavLink to="/" className={({ isActive }) => (isActive ? "text-blue-500" : "text-gray-700")}>Home</NavLink>
-        <NavLink to="addProduct" className={({ isActive }) => (isActive ? "text-blue-500" : "text-gray-700")}>Add Product</NavLink>
-        <NavLink to="allProduct" className={({ isActive }) => (isActive ? "text-blue-500" : "text-gray-700")}>All Product</NavLink>
-        <NavLink to="signUp" className={({ isActive }) => (isActive ? "text-blue-500" : "text-gray-700")}>Sign Up</NavLink>
-        <NavLink to="signIn" className={({ isActive }) => (isActive ? "text-blue-500" : "text-gray-700")}>Sign In</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-gray-700"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/allProduct"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-gray-700"
+          }
+        >
+          All Product
+        </NavLink>
+        <NavLink
+          to="/signUp"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-gray-700"
+          }
+        >
+          Sign Up
+        </NavLink>
+        <NavLink
+          to="/signIn"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-gray-700"
+          }
+        >
+          Sign In
+        </NavLink>
+        <NavLink
+          to="/addProduct"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-gray-700"
+          }
+        >
+          Add Product
+        </NavLink>
       </nav>
-      
+
       {/* Outlet to render nested routes */}
       <Outlet />
     </div>
